98bb8924797c90a16d9c9d26be85a2cfb3cf02d0
Kirthika-Ashokkumar <146607403+Kirthika-Ashokkumar@users.noreply.github.com>
2024-08-11 11:24:30 -0700

    Updated Science and Drivers Repo (#6)
    * moving science to urc 2024 rep
    
    * :wrench: Adding configurations for clangd and clang-format
    
    * updated modst files to libhal 3.0
    
    * updated scince except demos to libhal 3.0
    
    * new file structure created for drivers and updated science with new include paths so far building
    
    * rest of changes to updating file structure specifically to drivers but demos in drivers needs update

---
+++ drivers/conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+
+required_conan_version = ">=2.0.14"
+
+
+class demos(ConanFile):
+    python_requires = "libhal-bootstrap/[^2.1.1]"
+    python_requires_extend = "libhal-bootstrap.demo"
+
+    def requirements(self):
+        bootstrap = self.python_requires["libhal-bootstrap"]
+        bootstrap.module.add_demo_requirements(self)
---
+++ science/applications/application.hpp
@@ -0,0 +1,50 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <libhal/adc.hpp>
+#include <libhal/can.hpp>
+#include <libhal/functional.hpp>
+#include <libhal/i2c.hpp>
+#include <libhal/input_pin.hpp>
+#include <libhal/pwm.hpp>
+#include <libhal/serial.hpp>
+#include <libhal/steady_clock.hpp>
+
+// #include <implementations/mission_control.hpp>
+#include "../drivers/include/pump_manager.hpp"
+#include "../drivers/include/revolver.hpp"
+
+
+namespace sjsu::science {
+struct hardware_map_t
+{
+  pump_manager* pump_controller;
+
+  hal::servo* mixing_servo;
+  revolver* revolver_controller;
+
+  hal::steady_clock* steady_clock;
+  hal::serial* terminal;
+  // mission_control* mc;
+  hal::callback<void()> reset;
+};
+
+// Application function must be implemented by one of the compilation units
+// (.cpp) files.
+hardware_map_t initialize_platform();
+void application(hardware_map_t& p_framework);
+
+}  // namespace sjsu::science
