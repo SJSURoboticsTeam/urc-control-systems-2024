08160ee83d93e86063af659b1f4d1abd9227bba7
Kirthika-Ashokkumar <akirthika659@gmail.com>
2025-01-31 12:57:16 -0800

    added micromid to sources in cmake

--- drivers/src/tmag5273.cpp
+++ drivers/src/tmag5273.cpp
@@ -11,21 +11,18 @@ tmag5273::tmag5273(hal::i2c& p_i2c, hal::steady_clock& p_clock)
 
 void tmag5273::defualt_config()
 {
-  hal::byte dev_config_1 = 0x0C; //00001100
-  hal::byte dev_config_2 = 0x0A;//00001010
+  hal::byte dev_config_1 = 0x0C;  // 00001100
+  hal::byte dev_config_2 = 0x0A;  // 00001010
   tmag5273::config_device(dev_config_1, dev_config_2);
 
-  hal::byte sens_config_1 = 0x70; //01110000
-  hal::byte sens_config_2 = 0x08; // 00001000
+  hal::byte sens_config_1 = 0x70;  // 01110000
+  hal::byte sens_config_2 = 0x08;  // 00001000
 
   tmag5273::config_sensor(sens_config_1, sens_config_2);
 
-
-  //temp sens 
-  std::array<hal::byte, 3> write_buff = { device_addresses::T_CONFIG,
-                                          0x01};
+  // temp sens
+  std::array<hal::byte, 3> write_buff = { device_addresses::T_CONFIG, 0x01 };
   hal::write(m_i2c, device_addresses::i2c_address, write_buff);
-
 }
 
 void tmag5273::config_device(hal::byte config_1, hal::byte config_2)
@@ -33,17 +30,15 @@ void tmag5273::config_device(hal::byte config_1, hal::byte config_2)
   std::array<hal::byte, 3> write_buff = { device_addresses::DEVICE_CONFIG_1,
                                           config_1,
                                           config_2 };
-hal::
-  write(m_i2c, device_addresses::i2c_address, write_buff);
+  hal::write(m_i2c, device_addresses::i2c_address, write_buff);
 }
 
 void tmag5273::config_sensor(hal::byte config_1, hal::byte config_2)
 {
   std::array<hal::byte, 3> write_buff = { device_addresses::SENSOR_CONFIG_1,
-                                          config_1, config_2
- };
-hal::
-  write(m_i2c, device_addresses::i2c_address, write_buff);
+                                          config_1,
+                                          config_2 };
+  hal::write(m_i2c, device_addresses::i2c_address, write_buff);
 }
 
 tmag5273::data tmag5273::read()
@@ -65,14 +60,12 @@ tmag5273::data tmag5273::read()
   ret.y_field = (y * range) / 32768.0;
   int16_t z = buffer[6] << 8 | buffer[7];
   ret.z_field = (z * range) / 32768.0;
-  
+
   int16_t angle_val = (buffer[9] << 8 | buffer[10]) >> 4;
   float angle_dec = (buffer[10] & 0b0001111) / 16.0;
   ret.angle = angle_val + angle_dec;
 
   ret.magnitude = buffer[11];
-
-  
 
   return ret;
 }
