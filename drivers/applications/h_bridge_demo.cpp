517cc8a47e0b9fb841102679aa91d885c7658f37
vihashah <viha.shah@sjsu.edu>
2025-10-11 16:43:11 -0700

    slight refactor

--- drivers/applications/h_bridge_demo.cpp
+++ drivers/applications/h_bridge_demo.cpp
@@ -8,31 +8,13 @@ namespace sjsu::drivers {
 void application()
 {
   auto terminal = resources::console();
-  hal::print(*terminal, "Hey beginnning application\n");
-
-
   auto a_high = resources::pwm_channel_0();
-  hal::print(*terminal, "Getting a high\n");
-
   auto b_high = resources::pwm_channel_1();
   auto pwm_freq = resources::pwm_frequency();
-
-  hal::print(*terminal, "Getting b high\n");
-
   auto a_low = resources::a_low();
-  hal::print(*terminal, "pwm channel 0\n");
-
   auto b_low = resources::b_low();
-
-  hal::print(*terminal, "pwm channel 1\n");
-
   auto clock = resources::clock();
-
-  hal::print(*terminal, "clock\n");
-
-  auto m_h_bridge = h_bridge(a_high, b_high, a_low, b_low);
-
-  hal::print(*terminal, "h_brid\n");
+  auto m_h_bridge = h_bridge({ a_high, a_low }, { b_high, b_low });
 
   while (true) {
 
@@ -42,7 +24,6 @@ void application()
     hal::delay(*clock, 2000ms);
     m_h_bridge.power(-0.2);
     hal::delay(*clock, 2000ms);
-
   }
 }
 }  // namespace sjsu::drivers
