b4e68ac66355baf8593a1ac9aff5a6de985074c4
yyaatt0 <yathien122@gmail.com>
2025-10-24 18:41:03 -0700

    added gimble

---
+++ hub/include/gimble.hpp
@@ -0,0 +1,115 @@
+#pragma once
+#include <algorithm>
+#include <cmath>
+
+#include <libhal-actuator/rc_servo.hpp>    // For the two servos
+#include <libhal-sensor/imu/icm20948.hpp>  // Need Gyro, Accel, and Compass
+#include <libhal/units.hpp>
+
+namespace sjsu::hub {
+
+struct gimble_control_settings
+{
+  static constexpr float tau = 0.75;  // For the complementary filter
+  static constexpr float pi = 3.14159265f;
+
+  // A PI Controller will be applied to the m_y_servo (up/down or pitch)
+  static constexpr float Kp_pitch = 1.2f;
+  static constexpr float Ki_pitch = 0.6f;
+  static constexpr float Kd_pitch = 0.0f;
+
+  // Some constants if Mission Controls ever wants to apply PID onto the
+  // m_x_servo
+  static constexpr float Kp_yaw = 0.0f;
+  static constexpr float Ki_yaw = 0.0f;
+  static constexpr float Kd_yaw = 0.0f;
+
+  static constexpr hal::degrees max_servo_step =
+    30.0;  // Maximum step the servo can take during PID
+  static constexpr hal::degrees deg_tolerance = 5
+};
+
+struct pid_error
+{
+  float Ep;  // Error on the proportional gain
+  float Ei;  // Error on the integral gain
+  float Ed;  // Error on the derivative gain
+}
+
+class gimble
+{
+public:
+  /**
+   * @param p_x_servo the servo motor used to move the mast camera left and
+   * right (no PID control on it)
+   * @param p_y_servo the servo motor used to move the mast camera up and down
+   * (yes PID control on it)
+   * @param p_icm sensor needed to read the following: accelerometer, compass,
+   * and gyroscope
+   * @param p_min_angle minimum angle set inside the rc_servo_settings for both
+   * the servo
+   * @param p_max_angle maximum angle set inside the rc_servo_settings for both
+   * the servo
+   */
+  gimble(hal::v5::strong_ptr<hal::actuator::rc_servo> p_x_servo,
+         hal::v5::strong_ptr<hal::actuator::rc_servo> p_y_servo,
+         hal::v5::strong_ptr<hal::sensor::icm20948> p_icm,
+         hal::degrees p_min_angle,
+         hal::degrees p_max_angle);
+
+  /**
+   * @brief The function will move the gimble towards the left by p_degrees (no
+   * PID)
+   *
+   * @param p_degree the # of degrees to move the servo in the left direction
+   */
+  void move_left(hal::degrees p_degree);
+
+  /**
+   * @brief The function will move the gimble towards the right by p_degrees (no
+   * PID)
+   *
+   * @param p_degree the # of degrees to move the servo in the right direction
+   */
+  void move_right(hal::degrees p_degree);
+
+  /**
+   * @brief The function will update the target_pitch by p_degree and
+   * update_y_servo will update the gimble's oreitnation to match the
+   *
+   * @param p_degree the # of degrees to move the servo in the up direction
+   */
+  void move_up(hal::degrees p_degree);
+
+  /**
+   * @brief The function will update the target_pitch by p_degree and
+   * update_y_servo will update the gimble's oreitnation to match the
+   *
+   * @param p_degree the # of degrees to move the servo in the down direction
+   */
+  void move_down(hal::degrees p_degree);
+
+  /**
+   * @brief The function will update the m_y_servo where the error between it's
+   * current pitch and target pitch is zero (PID)
+   *
+   * @param p_delta_time the time in seconds from the previous call of this
+   * function and the current call of this function
+   */
+  void update_y_servo(float p_delta_time);
+
+private:
+  hal::v5::strong_ptr<hal::actuator::rc_servo> m_x_servo;
+  hal::v5::strong_ptr<hal::actuator::rc_servo> m_y_servo;
+  hal::v5::sttrong_ptr<hal::sensoq::icm20948> m_icm;
+  hal::degrees m_min_angle, m_max_angle;
+
+  hal::degrees curr_y_servo_angle, curr_x_servo_angle;
+
+  float tar_pitch, curr_pitch, filtered_pitch;  // For PID; in degrees
+  // float tar_yaw, curr_pitch; // Incase Mission Control wants PID on the yaw
+  // of the gimble
+
+  struct pid_error error;
+};
+}  // namespace sjsu::hub
---
+++ hub/src/gimble.cpp
@@ -0,0 +1,162 @@
+#pragma once
+#include <gimble.hpp>
+
+namespace sjsu::hub {
+
+gimble::gimble(hal::v5::strong_ptr<hal::actuator::rc_servo> p_x_servo,
+               hal::v5::strong_ptr<hal::actuator::rc_servo> p_y_servo,
+               hal::v5::strong_ptr<hal::sensor::icm20948> p_icm,
+               hal::degrees p_min_angle,
+               hal::degrees p_max_angle)
+  : m_x_servo(p_x_servo)
+  , m_y_servo(p_y_servo)
+  , m_icm(p_icm)
+  , m_min_angle(p_min_angle)
+  , m_max_angle(p_max_angle)
+{
+  mid_angle = (m_min_angle + m_max_angle) / 2.0;
+
+  // Setting starting positions for the servos
+  curr_x_servo_angle = mid_angle;
+  curr_y_servo_angle = mid_angle;
+
+  // Setting the servos at some known location
+  m_x_servo->driver_position(curr_x_servo_angle);
+  m_y_servo->driver_position(curr_y_servo_angle);
+
+  // Default
+  tar_pitch = 0.0f;
+  filtered_pitch = 0.0f;
+
+  // Error
+  error.Ep = 0.0f;
+  error.Ei = 0.0f;
+  error.Ed = 0.0f;
+}
+
+void gimble::move_left(hal::degrees p_degree)
+{
+  // Based on how the servo motor is fixed, the left could be moving towards
+  // m_min_angle or m_max_angle
+
+  // Delete the one that it actually goes to
+
+  // If left means going to m_min_angle
+  if (curr_x_servo_angle - p_degree < m_min_angle)
+    curr_x_servo_angle = m_min_angle;
+  else
+    curr_x_servo_angle -= p_degree;
+
+  // If left means going to m_max_angle
+  if (curr_x_servo_angle + p_degree > m_max_angle)
+    curr_x_servo_angle = m_max_angle;
+  else
+    curr_x_servo_angle += p_degree;
+
+  // Update the servo position
+  m_x_servo->driver_position(curr_x_servo_angle);
+}
+
+void gimble::move_right(hal::degrees p_degree)
+{
+  // Based on how the servo motor is fixed, the right could be moving towards
+  // m_min_angle or m_max_angle
+
+  // Delete the one that it actually goes to
+
+  // If left means going to m_min_angle
+  if (curr_x_servo_angle - p_degree < m_min_angle)
+    curr_x_servo_angle = m_min_angle;
+  else
+    curr_x_servo_angle -= p_degree;
+
+  // If left means going to m_max_angle
+  if (curr_x_servo_angle + p_degree > m_max_angle)
+    curr_x_servo_angle = m_max_angle;
+  else
+    curr_x_servo_angle += p_degree;
+
+  // Update the servo position
+  m_x_servo->driver_position(curr_x_servo_angle);
+}
+
+void gimble::move_up(hal::degrees p_degree)
+{
+  // Based on how the servo motor is fixed, the up could be moving towards
+  // m_min_angle or m_max_angle
+
+  // Have to look into this cause I don't know the range of pitch given the ICM
+  tar_pitch += p_degree;
+}
+
+void gimble::move_down(hal::degrees p_degree)
+{
+  // Based on how the servo motor is fixed, the left could be moving towards
+  // m_min_angle or m_max_angle
+
+  // Have to look into this cause I don't know the range of pitch given the ICM
+  tar_pitch -= p_degree;
+}
+
+void gimble::update_servo(float p_delta_time)
+{
+  float alpha = gimble_control_settings::tau /
+                (tau + p_delta_time);  // Used for the complementary filter
+
+  // Reading the raw values from the sensor
+  auto raw_accel = m_icm->read_acceleration();
+  auto raw_gyro = m_icm->read_gyroscope();
+
+  // Getting the pitch
+  float angle_pitch_deg =
+    atan2f(raw_accel.x,
+           sqrtf(raw_accel.y * raw_accel.y + raw_accel.z * raw_accel.z)) *
+    180.0f / gimble_control_setting::pi;
+
+  // Cleaning the pitch value from noise
+  filtered_pitch = alpha * (filtered_pitch + raw_gyro.y * p_delta_time) +
+                   (1.0f - alpha) * angle_pitch_deg;
+
+  // Getting the error
+  float curr_error = filtered_pitch - tar_pitch;
+
+  // Checking if the current error will make the angle position set for the
+  // servo out of bounds (the servo will fail)
+  bool at_upper = (curr_y_servo_angle >= m_max_angle - 0.001f);
+  bool at_lower = (curr_y_servo_angle >= m_min_angle + 0.001f);
+
+  bool pushing_upper = at_upper && (e_pitch > 0);
+  bool pushing_lower = at_lower && (e_pitch < 0);
+
+  if (!pushing_upper && !pushing_lower)
+    error.Ei += curr_error * p_delta_time;
+
+  error.Ed = (curr_error - error.Ep) / p_delta_time;
+  error.Ep = curr_error;
+
+  // Calculating the offset to help the ICM sensor reach steady state
+  // (tar_pitch)
+  float control_var = (gimble_control_settings::Kp * error.Ep) +
+                      (gimble_control_settings::Ki * error.Ei) +
+                      (gimble_control_settings::Kd * error.Ed);
+
+  // Ensure that we aren't going above the servo's range
+  control_var =
+    std::clamp(control_var,
+               m_min_angle + gimble_control_settings::deg_tolerance,
+               m_max_angle + gimble_control_settings::deg_tolerance);
+
+  float new_angle_pos = (m_min_angle + m_max_angle) / 2 + control_var;
+
+  new_angle_pos = std::clamp(new_angle_pos, m_min_angle, m_max_angle);
+  new_angle_pos =
+    std::clamp(new_angle_pos,
+               curr_y_servo_angle - gimble_control_settings::max_servo_step,
+               curr_y_servo_angle + gimble_control_settings::max_servo_step);
+  new_angle_pos = std::clamp(new_angle_pos, m_min_angle, m_max_angle);
+
+  // Updating the servo position
+  m_y_servo->driver_position(new_angle_pos);
+  curr_y_servo_angle = new_angle_pos;
+}
+}  // namespace sjsu::hub
