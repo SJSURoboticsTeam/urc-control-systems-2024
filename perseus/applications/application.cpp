9493df056dfbf90ff64363e173ea3cc3b7026169
vihashah <viha.shah@sjsu.edu>
2025-11-23 13:09:16 -0800

    removed

--- perseus/applications/application.cpp
+++ perseus/applications/application.cpp
@@ -12,7 +12,6 @@
 
 #include "../hardware_map.hpp"
 
-#include "../include/can_messaging.hpp"
 #include "../include/bldc_servo.hpp"
 
 
@@ -25,32 +24,6 @@ namespace sjsu::perseus {
 // 188:1 elbow 1-2 reduction. 5281.1 * 2
 void application()
 {
-  using namespace std::chrono_literals;
-  using namespace hal::literals;
-  auto console = resources::console();
-  auto clock = resources::clock();
-  auto h_bridge = resources::h_bridge();
-  auto encoder = resources::encoder();
-  auto can_transceiver = resources::can_transceiver();
-  auto bus_manager = resources::can_bus_manager();
-  bus_manager->baud_rate(1.0_MHz);
-  auto can_id_filter = resources::can_identifier_filter();
-  hal::u16 servo_address = can_perseus::servo_address::track_servo;
-  hal::can_message_finder can_finder(*can_transceiver, 0x110);
-
-  can_id_filter->allow(0x110);
-  hal::print(*console, "CAN message finder initialized...\n");
-  bldc_perseus servo(h_bridge, encoder);
-  hal::print(*console, "BLDC Servo created...\n");
-  can_perseus servo_can(servo_address); 
-  hal::print(*console, "Servo can creature setup...\n");
-
-  auto servo_ptr = hal::v5::make_strong_ptr<decltype(servo)>(resources::driver_allocator(), std::move(servo));
-  
-  hal::print(*console, "BLDC Servo initialized...\n");
-
-  while (true) {
-    // TODO!
-  } 
+  // TODO!
 }
 }  // namespace sjsu::perseus
