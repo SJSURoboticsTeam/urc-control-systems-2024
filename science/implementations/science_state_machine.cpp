0eab9c38cc4202b5281bd38b548c3bd14f28a103
AnnaT0T <trananna3018@gmail.com>
2025-12-20 16:37:48 -0800

    Initial states (#80)

--- science/implementations/science_state_machine.cpp
+++ science/implementations/science_state_machine.cpp
@@ -1,5 +1,5 @@
 #include "../include/science_state_machine.hpp"
-
+using namespace std::chrono_literals;
 namespace sjsu::science {
 science_state_machine::science_state_machine(
   hal::actuator::rc_servo p_arm_servo,
@@ -32,6 +32,43 @@ void science_state_machine::turn_on_pump([[maybe_unused]] auto pump, [[maybe_unu
 
 void science_state_machine::run_state_machine([[maybe_unused]] science_states state)
 {
+  switch(state){
+    case science_state_machine::science_states::HOME_CAROUSEL:
+      break;
+    case science_state_machine::science_states::CUP_OUTSIDE:
+      m_door.position(180);
+      hal::delay(*m_clock, 1000ms);
+      m_arm_servo.position(180);
+      hal::delay(*m_clock, 1000ms);
+      break;
+    case science_state_machine::science_states::CUP_INSIDE:
+      m_arm_servo.position(0);
+      hal::delay(*m_clock, 1000ms);
+      m_door.position(0);
+      hal::delay(*m_clock, 1000ms);
+      break;
+    case science_state_machine::science_states::DUMP_SAMPLE: 
+      // trapdoor servo deg: 0 close, 120 open)
+      m_trap_door.position(0);
+      hal::delay(*m_clock, 1000ms);
+      m_trap_door.position(120); // keep open for 3s
+      hal::delay(*m_clock, 3000ms);
+      m_trap_door.position(0);
+      hal::delay(*m_clock, 1000ms);
+      break;
+    case science_state_machine::science_states::ADD_DI_WATER:
+      break;
+    case science_state_machine::science_states::MIX_DI_WATER:
+      break;
+    case science_state_machine::science_states::ADD_MIX_BENEDICT_REAGENT:
+      break;
+    case science_state_machine::science_states::ADD_BIURET_REAGENT:
+      break;
+    case science_state_machine::science_states::ADD_KALLING_REAGENT:
+      break;
+    case science_state_machine::science_states::READ_COLOR_SENSOR:
+      break;
+  }
 }
 
 }  // namespace sjsu::science
