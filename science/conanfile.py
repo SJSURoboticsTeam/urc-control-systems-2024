89406dadc9068d3307399424554b3248172b3859
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2026-01-21 11:42:48 -0800

    update arm-mcu-version & remove science artifacts

--- science/conanfile.py
+++ science/conanfile.py
@@ -4,7 +4,7 @@ required_conan_version = ">=2.0.14"
 
 
 class demos(ConanFile):
-    python_requires = "libhal-bootstrap/[>=4.3.0 <5]"
+    python_requires = "libhal-bootstrap/[4.3.0]"
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
@@ -13,7 +13,7 @@ class demos(ConanFile):
         # self.requires("libhal-canrouter/[3.0.0]")
         self.requires("libhal/4.18.1")
         self.requires("libhal-util/5.8.1")
-        self.requires("libhal-arm-mcu/1.19.1")
+        self.requires("libhal-arm-mcu/1.19.6")
         self.requires("libhal-actuator/1.2.3")
 
 
