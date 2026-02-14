4b99c1501ad407244440c014fa8f52d030381d49
Kirthika-Ashokkumar <akirthika659@gmail.com>
2026-02-14 14:11:58 -0800

    no longer using c-array

--- science/applications/load_cell_demo.cpp
+++ science/applications/load_cell_demo.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cinttypes>
 
 #include <libhal-util/serial.hpp>
@@ -7,20 +8,22 @@
 #include <tuple>
 
 const int sample_rate = 20;
-const float filter_const[20]=  {9.66174445e-05, 1.55986763e-04, 3.33618583e-04, 7.15587299e-04,
- 1.46574468e-03, 2.93946352e-03, 6.05407192e-03, 1.38743094e-02,
- 4.26119415e-02, 4.02822938e-01, 4.02822938e-01, 4.26119415e-02,
- 1.38743094e-02, 6.05407192e-03, 2.93946352e-03, 1.46574468e-03,
- 7.15587299e-04, 3.33618583e-04, 1.55986763e-04, 9.66174445e-05};
+const float filter_const[20] = { 9.66174445e-05, 1.55986763e-04, 3.33618583e-04,
+                                 7.15587299e-04, 1.46574468e-03, 2.93946352e-03,
+                                 6.05407192e-03, 1.38743094e-02, 4.26119415e-02,
+                                 4.02822938e-01, 4.02822938e-01, 4.26119415e-02,
+                                 1.38743094e-02, 6.05407192e-03, 2.93946352e-03,
+                                 1.46574468e-03, 7.15587299e-04, 3.33618583e-04,
+                                 1.55986763e-04, 9.66174445e-05 };
 
 using namespace std::chrono_literals;
 namespace sjsu::science {
 
-float filter(float arr[sample_rate], int begin)
+float filter(std::array<float, sample_rate> arr, int begin)
 {
   float sum = 0;
-  for(int i = 0; i < sample_rate; i++) {
-    int idx = (begin + i)% sample_rate;
+  for (uint16_t i = 0; i < sample_rate; i++) {
+    int idx = (begin + i) % sample_rate;
     sum += (arr[idx] * filter_const[i]);
   }
   return sum;
@@ -34,15 +37,16 @@ void application()
 
   hal::print(*console, "ADC Application Starting...\n");
 
-  float adc_data[sample_rate]= {0};
+  // float adc_data[sample_rate] = { 0 };
+  std::array<float, sample_rate> adc_data;
+  adc_data.fill(0);
 
   int begin = 0;
 
   while (true) {
     adc_data[begin] = adc->read();
     hal::print<128>(
-      *console,
-      "LOAD CELL PERCENT: %f\n",filter(adc_data, begin));
+      *console, "LOAD CELL PERCENT: %f\n", filter(adc_data, begin));
     hal::delay(*clock, 100ms);
     begin++;
 
