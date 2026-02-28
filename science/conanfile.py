f168c39e979a30e0e45d733082883e1163b6feaa
AnnaT0T <184180343+AnnaT0T@users.noreply.github.com>
2026-02-28 13:50:41 -0800

    Hardware init science (#94)
    - defined state machine objects
    - Hamza helped with some definitions + debugged pca

--- science/conanfile.py
+++ science/conanfile.py
@@ -15,5 +15,6 @@ class demos(ConanFile):
         self.requires("libhal-util/5.8.1")
         self.requires("libhal-arm-mcu/1.19.6")
         self.requires("libhal-actuator/1.2.3")
+        self.requires("libhal-expander/1.3.1")
 
 
