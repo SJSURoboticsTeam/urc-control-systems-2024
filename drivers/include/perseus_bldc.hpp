a46efd4fb3fe47cb6d69a81d5d9f6e3fe4be9c61
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2026-08-12 17:43:37 -0700

    bare bones perseus driver

--- drivers/include/perseus_bldc.hpp
+++ drivers/include/perseus_bldc.hpp
@@ -1,17 +1,22 @@
-//template from drivers/include/h_bridge.hpp
+// template from drivers/include/h_bridge.hpp
 
 #pragma once
 #include <array>
-#include <libhal/pointers.hpp>
+#include <libhal-util/can.hpp>
+#include <libhal/can.hpp>
 #include <libhal/motor.hpp>
 #include <libhal/output_pin.hpp>
+#include <libhal/pointers.hpp>
 #include <libhal/pwm.hpp>
+#include <libhal/steady_clock.hpp>
 #include <libhal/units.hpp>
 
+namespace sjsu::drivers {
 
-namespace sjsu::perseus {
+using namespace std::chrono_literals;
 
-struct PidSettings // A structure to hold PID (Proportional, Integral, Derivative)
+struct pid_settings  // A structure to hold PID (Proportional, Integral,
+                     // Derivative)
 {
   float kp = 0.0f;
   float ki = 0.0f;
@@ -21,18 +26,125 @@ struct PidSettings // A structure to hold PID (Proportional, Integral, Derivativ
 class perseus_bldc
 {
 public:
-  perseus_bldc() = default;  
-  
-  void set_encoder_position(hal::degrees position);
-  void set_percent_voltage(float percent);
-  void set_pid(const PidSettings& p_settings);
+  perseus_bldc(hal::v5::strong_ptr<hal::can_transceiver> p_can_transceiver,
+               hal::v5::strong_ptr<hal::steady_clock> p_clock,
+               hal::u32 p_can_id,
+               hal::time_duration p_max_response_time = 500ms);
+  // currently inorder of messages on CAN ID doc
+  /**
+   * @brief kills power, sets voltage to 0
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void kill_power();
+  /**
+   * @brief used to check that perseus motor communication is responsive
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void heart_beat();
+  /**
+   * @brief run homing sequence to adjust position
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void home();
+  /**
+   * @brief check if currently running a homing sequence
+   * @returns if currently running a homing sequence
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  bool is_homing();
+  /**
+   * @brief set target position
+   * @param p_target_position new target of positional pid
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_target_position(hal::degrees p_target_position);
+  /**
+   * @brief check if target position has been hit since target position was
+   * given
+   * @returns if target position has been hit since target position was given
+   * (also true if no target position is active)
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  bool hit_target_position();
+  /**
+   * @brief sets encoder position to make new frame of reference
+   * @param p_position postion is in new frame of reference
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_encoder_position(hal::degrees p_position);
+  /**
+   * @brief set target velocity
+   * @param p_target_velocity new target of velocity pid
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_target_velocity(hal::rpm p_target_velocity);
+  /**
+   * @brief set voltage as portion of max
+   * @param p_portion motor voltage as portion of max voltage, must be a value
+   * from -1 to 1 (inclusive)
+   * @throws hal::argument_out_of_domain - p_portion was not from -1 to 1
+   * (inclusive)
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_power(float p_portion);
+  /**
+   * @brief set the constants for positional PID
+   * @param p_settings new positional PID constants
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_position_pid_config(pid_settings const& p_settings);
+  /**
+   * @brief set the constants for velocity PID
+   * @param p_settings new velocity PID constants
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void set_velocity_pid_config(pid_settings const& p_settings);
+  /**
+   * @brief get target position
+   * @returns target position of positional PID
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  hal::degrees get_target_position();
+  /**
+   * @brief get position
+   * @returns encoder position in established frame of reference
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
   hal::degrees get_position();
-  hal::degrees get_velocity();
-  void set_position(hal::degrees degrees);
-  void set_velocity(float rpm);
-  // void set_feedforward(float );
-  void reset_factor();
-  void set_gear_ratio(float p_ratio);
+  // hal::rpm get_target_velocity();
+  /**
+   * @brief get velocity based on encoder reading
+   * @returns velocity based on encoder reading
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  hal::rpm get_velocity();
+  /**
+   * @brief get voltage portion of max being supplied to motor
+   * @returns voltage portion of max being supplied to motor
+   */
+  float read_power();
+  /**
+   * @brief get the constants for positional PID
+   * @param p_settings positional PID constants
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void get_position_pid_config(pid_settings const& p_settings);
+  /**
+   * @brief get the constants for velocity PID
+   * @param p_settings velocity PID constants
+   * @throws hal::timed_out - perseus motor controller doesn't respond
+   */
+  void get_velocity_pid_config(pid_settings const& p_settings);
 
+private:
+  hal::v5::strong_ptr<hal::can_transceiver> m_can_transceiver;
+  hal::v5::strong_ptr<hal::steady_clock> m_clock;
+  hal::u32 m_can_id;
+  hal::can_message_finder m_reply_message_finder;
+  hal::time_duration m_max_response_time;
+
+  hal::can_message send(
+    std::array<hal::byte, 8> const& p_payload,
+    hal::byte const& length);
 };
 }  // namespace sjsu::drivers
---
+++ drivers/src/perseus_bldc.cpp
@@ -0,0 +1,201 @@
+#include <can_util.hpp>
+#include <cstdint>
+#include <cstdlib>
+#include <libhal-util/serial.hpp>
+#include <libhal-util/steady_clock.hpp>
+#include <libhal/can.hpp>
+#include <libhal/error.hpp>
+#include <libhal/units.hpp>
+#include <optional>
+#include <perseus_bldc.hpp>
+#include <resource_list.hpp>
+#include <span>
+
+namespace {
+enum class action : hal::byte
+{
+  // top priority
+  power_off_reset = 0x0C,  // hard stop the servo to be 0
+  heartbeat = 0x0E,        // are you alive
+
+  // actuators
+  homing = 0x11,
+  set_position_target = 0x12,
+  set_position_reading = 0x13,
+  set_velocity_target = 0x14,
+  set_power = 0x16,
+  set_pid_position_config = 0x17,
+  set_pid_velocity_config = 0x18,
+
+  // readers
+  read_homing_status = 0x21,
+  read_position_target = 0x22,
+  read_position_reading = 0x23,
+  read_velocity_target = 0x24,
+  read_velocity_reading = 0x25,
+  read_power = 0x26,
+  read_pid_position_config = 0x27,
+  read_pid_velocity_config = 0x28,
+
+  // servo to servo
+  prev_joint_actual_position = 0x41,
+  prev_joint_position_response = 0x51,
+};
+};
+
+namespace sjsu::drivers {
+
+// TODO: implement unimplemented functions
+
+perseus_bldc::perseus_bldc(
+  hal::v5::strong_ptr<hal::can_transceiver> p_can_transceiver,
+  hal::v5::strong_ptr<hal::steady_clock> p_clock,
+  hal::u32 p_can_id,
+  hal::time_duration p_max_response_time)
+  : m_can_transceiver(p_can_transceiver)
+  , m_clock(p_clock)
+  , m_can_id(p_can_id)
+  , m_reply_message_finder(
+      hal::can_message_finder(*p_can_transceiver, p_can_id + 0x100))
+  , m_max_response_time(p_max_response_time)
+{
+  // throw hal::operation_not_supported(this);
+}
+
+void perseus_bldc::kill_power()
+{
+  // throw hal::operation_not_supported(this);
+  set_power(0);
+}
+void perseus_bldc::heart_beat()
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::home()
+{
+  send({ static_cast<hal::byte>(action::homing) }, 1);
+}
+bool perseus_bldc::is_homing()
+{
+  auto reply = send({ static_cast<hal::byte>(action::read_homing_status) }, 1);
+  return reply.payload[1];
+}
+void perseus_bldc::set_target_position(hal::degrees p_target_position)
+{
+  // auto console = resources::console();
+  int8_t exponent = 14;
+  auto target_position_int =
+    can_util::floating_to_fixed_point_32(p_target_position / 360.0f, exponent);
+  // hal::print<64>(*console, "Seting Target Int: %d\n", target_position_int);
+  auto target_position_array =
+    can_util::int32_to_byte_array_big_endian(target_position_int);
+  send(
+    {
+      static_cast<hal::byte>(action::set_position_target),
+      static_cast<hal::byte>(exponent),
+      target_position_array[0],
+      target_position_array[1],
+      target_position_array[2],
+      target_position_array[3],
+    },
+    6);
+}
+bool perseus_bldc::hit_target_position()
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::set_target_velocity(hal::rpm p_target_velocity
+                                       [[maybe_unused]])
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::set_power(float p_portion)
+{
+  // auto console = resources::console();
+  // hal::print<64>(*console, "Seting Power: %f\n",p_portion);
+  bool sign = p_portion < 0;
+  uint16_t duty_cycle = std::abs(p_portion) * 0xFFFF;
+  // hal::print<64>(*console, "Duty Cycle: %x\n",duty_cycle);
+  auto duty_cycle_array = can_util::int16_to_byte_array_big_endian(duty_cycle);
+  send({ static_cast<hal::byte>(action::set_power),
+         sign,
+         duty_cycle_array[0],
+         duty_cycle_array[1] },
+       4);
+}
+void perseus_bldc::set_position_pid_config(pid_settings const& p_settings
+                                           [[maybe_unused]])
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::set_velocity_pid_config(pid_settings const& p_settings
+                                           [[maybe_unused]])
+{
+  throw hal::operation_not_supported(this);
+}
+hal::degrees perseus_bldc::get_target_position()
+{
+  throw hal::operation_not_supported(this);
+}
+hal::degrees perseus_bldc::get_position()
+{
+  auto message =
+    send({ static_cast<hal::byte>(action::read_position_reading) }, 1);
+  return can_util::fixed_to_floating_point_32(
+           can_util::byte_array_to_int32_big_endian({ message.payload[2],
+                                           message.payload[3],
+                                           message.payload[4],
+                                           message.payload[5] }),
+           message.payload[1]) *
+         360.0f;
+}
+hal::rpm perseus_bldc::get_velocity()
+{
+  throw hal::operation_not_supported(this);
+}
+float perseus_bldc::read_power()
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::get_position_pid_config(pid_settings const& p_settings
+                                           [[maybe_unused]])
+{
+  throw hal::operation_not_supported(this);
+}
+void perseus_bldc::get_velocity_pid_config(pid_settings const& p_settings
+                                           [[maybe_unused]])
+{
+  throw hal::operation_not_supported(this);
+}
+
+hal::can_message perseus_bldc::send(std::array<hal::byte, 8> const& p_payload,
+                                    hal::byte const& length)
+{
+  hal::can_message const payload{
+    .id = m_can_id,
+    .length = length,
+    .payload = p_payload,
+  };
+  // auto console = resources::console();
+  // hal::print<64>(*console, "Sending Message\n");
+
+  // Send payload
+  m_can_transceiver->send(payload);
+  // auto console = resources::console();
+  // can_util::print_can_message(*console, payload);
+  // Wait for reply in case of time out
+  auto const deadline = hal::future_deadline(*m_clock, m_max_response_time);
+  while (deadline > m_clock->uptime()) {
+    // hal::print<64>(*console, "Waiting for message\n");
+    auto const message = m_reply_message_finder.find();
+    // hal::print<64>(*console, "Message Searched\n");
+    if (message.has_value()) {
+      // hal::print<64>(*console, "Message Found\n");
+      return message.value();
+    }
+  }
+  // hal::print<64>(*console, "Timed Out Throwing\n");
+  throw hal::timed_out(this);
+}
+
+};  // namespace sjsu::drivers
